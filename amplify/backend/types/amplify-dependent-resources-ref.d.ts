bc07b5bd65c67a7bebeb80fa35114b285697dc07
burenbayar.p <burenbayar.p@chinggis.systems>
2023-05-03 10:33:34 +0800

    init amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,23 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "edumatch03e42e4e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "coachGroupRole": "string",
+            "studentGroupRole": "string"
+        }
+    },
+    "api": {
+        "edumatch": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
